75b66e3b1cdb6748cd8bf0bdea2fb610ad14768a
BuildTools <unconfigured@null.spigotmc.org>
2021-01-12 13:00:22 -0800

    Feat: Create a button that navigates to /servers using a router injection

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
+  }
+
+  onLoadServers() {
+    // complex calculation or store on server.
+    this.router.navigate(['/servers']);
+    //an array of the single elements of the new path
   }
 
 }
