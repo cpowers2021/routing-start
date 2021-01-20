e3178743205f766f71ed5ad726f886bbe6a83fa1
BuildTools <unconfigured@null.spigotmc.org>
2021-01-19 22:02:28 -0800

    Feat: Add child routes for users and servers

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,11 +13,13 @@ import { EditServerComponent } from './servers/edit-server/edit-server.component
 import { ServerComponent } from './servers/server/server.component';
 import { ServersService } from './servers/servers.service';
 
-const appRoutes: Routes = [{path: 'users', component: UsersComponent},
-{path: 'users/:id/:name', component: UserComponent},
-{path: 'servers', component: ServersComponent},
-{path: 'servers/:id', component: ServerComponent
-{path: 'servers/:id/edit', component: EditServerComponent},
+const appRoutes: Routes = [{path: 'users', component: UsersComponent, children: [
+{path: ':id/:name', component: UserComponent}
+]},
+{path: 'servers', component: ServersComponent, children: [
+{path: ':id', component: ServerComponent},
+{path: ':id/edit', component: EditServerComponent}
+]},
 {path: '', component: HomeComponent}];
 
 @NgModule({
